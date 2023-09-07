780c514265a288742108319d7dca8fc2e618a58c
Bright Su <gt07814@greattree.com.tw>
2023-09-07 09:33:44 +0800

    feat: add blank themes

---
+++ www/wp-content/themes/blank/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Wordpress Demo</title>
+  <link rel="stylesheet" href="style.css">
+ </head>
+ <body>
+  <h2>Welcome to Wordpress.</h2>
+ </body>
+</html>
